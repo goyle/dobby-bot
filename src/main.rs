e1aef55e6ff557cf7b950e612f2015821bc1ddd8
goyle <62769624+goyle@users.noreply.github.com>
2020-05-24 20:59:45 +0000

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,179 @@
+mod commands;
+
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    sync::Arc,
+};
+use serenity::{
+    client::bridge::gateway::ShardManager,
+    framework::{
+        StandardFramework,
+        standard::macros::{command, group, help, check},
+        standard::{
+            Args, CheckResult, CommandOptions, CommandResult, CommandGroup,
+            DispatchError, HelpOptions, help_commands,
+        },
+    },
+    model::{channel::{Channel, Message}, id::UserId, event::ResumedEvent, gateway::Ready},
+    prelude::*,
+};
+use log::{error, info};
+
+use commands::{
+    meta::*,
+    owner::*,
+    grin::*,
+};
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct CommandCounter;
+
+impl TypeMapKey for CommandCounter {
+    type Value = HashMap<String, u64>;
+}
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
+    }
+
+    fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
+    }
+}
+
+#[group]
+#[commands(about, ping, quit)]
+struct General;
+
+#[group]
+#[commands(calculator, code_of_conduct, emission, explorers, governance, grin, miners, philosophy, roadmap, symbol, wallets)]
+struct Grin;
+
+// The framework provides two built-in help commands for you to use.
+// But you can also make your own customized help command that forwards
+// to the behaviour of either of them.
+#[help]
+// This replaces the information that a user can pass
+// a command-name as argument to gain specific information about it.
+#[individual_command_tip =
+"Hello! If you want more information about a specific command, just pass the command as argument."]
+// Some arguments require a `{}` in order to replace it with contextual information.
+// In this case our `{}` refers to a command's name.
+#[command_not_found_text = "Could not find: `{}`."]
+// Define the maximum Levenshtein-distance between a searched command-name
+// and commands. If the distance is lower than or equal the set distance,
+// it will be displayed as a suggestion.
+// Setting the distance to 0 will disable suggestions.
+#[max_levenshtein_distance(3)]
+// When you use sub-groups, Serenity will use the `indention_prefix` to indicate
+// how deeply an item is indented.
+// The default value is "-", it will be changed to "+".
+#[indention_prefix = "+"]
+// On another note, you can set up the help-menu-filter-behaviour.
+// Here are all possible settings shown on all possible options.
+// First case is if a user lacks permissions for a command, we can hide the command.
+#[lacking_permissions = "Hide"]
+// If the user is nothing but lacking a certain role, we just display it hence our variant is `Nothing`.
+#[lacking_role = "Nothing"]
+// The last `enum`-variant is `Strike`, which ~~strikes~~ a command.
+#[wrong_channel = "Strike"]
+// Serenity will automatically analyse and generate a hint/tip explaining the possible
+// cases of ~~strikethrough-commands~~, but only if
+// `strikethrough_commands_tip(Some(""))` keeps `Some()` wrapping an empty `String`, which is the default value.
+// If the `String` is not empty, your given `String` will be used instead.
+// If you pass in a `None`, no hint will be displayed at all.
+fn my_help(
+    context: &mut Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>
+) -> CommandResult {
+    help_commands::with_embeds(context, msg, args, help_options, groups, owners)
+}
+
+fn main() {
+    // This will load the environment variables located at `./.env`, relative to
+    // the CWD. See `./.env.example` for an example on how to structure this.
+    kankyo::load().expect("Failed to load .env file");
+
+    // Initialize the logger to use environment variables.
+    //
+    // In this case, a good default is setting the environment variable
+    // `RUST_LOG` to debug`.
+    env_logger::init();
+
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+
+    let prefix: &str = &env::var("PREFIX")
+        .expect("Expected a token in the environment");
+
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    {
+        let mut data = client.data.write();
+        data.insert::<CommandCounter>(HashMap::default());
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
+
+    let owners = match client.cache_and_http.http.get_current_application_info() {
+        Ok(info) => {
+            let mut set = HashSet::new();
+            set.insert(info.owner.id);
+
+            set
+        },
+        Err(why) => panic!("Couldn't get application info: {:?}", why),
+    };
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c
+            .owners(owners)
+            .prefix(prefix))
+        .before(|ctx, msg, command_name| {
+            println!("Got command '{}' by user '{}'",
+                        command_name,
+                        msg.author.name);
+            // Increment the number of times this command has been run once. If
+            // the command's name does not exist in the counter, add a default
+            // value of 0.
+            let mut data = ctx.data.write();
+            let counter  = data.get_mut::<CommandCounter>().expect("Expected CommandCounter in ShareMap.");
+            let entry = counter.entry(command_name.to_string()).or_insert(0);
+            *entry += 1;
+
+            true // if `before` returns false, command processing doesn't happen.
+        })
+        .after(|_, _, command_name, error| {
+            match error {
+                Ok(()) => println!("Processed command '{}'", command_name),
+                Err(why) => println!("Command '{}' returned error {:?}", command_name, why),
+            }
+        })
+        .unrecognised_command(|_, _, unknown_command_name| {
+            println!("Could not find command named '{}'", unknown_command_name);
+        })
+        .on_dispatch_error(|ctx, msg, error| {
+            if let DispatchError::Ratelimited(seconds) = error {
+                let _ = msg.channel_id.say(&ctx.http, &format!("Try this again in {} seconds.", seconds));
+            }
+        })
+        .help(&MY_HELP)
+        // Can't be used more than 1 times per 43200 seconds, with a 5 second delay:
+        .group(&GENERAL_GROUP)
+        .group(&GRIN_GROUP));
+
+    if let Err(why) = client.start() {
+        error!("Client error: {:?}", why);
+    }
+}

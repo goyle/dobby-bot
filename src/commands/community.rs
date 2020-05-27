ebf7801426a5541fb2a0c36af56a8a462f71d754
goyle <62769624+goyle@users.noreply.github.com>
2020-05-27 21:02:30 +0000

    Update community.rs

--- src/commands/community.rs
+++ src/commands/community.rs
@@ -1,1 +1,73 @@
+use std::path::Path;
 
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+use serenity::framework::standard::{
+    CommandResult,
+    macros::command,
+};
+use serenity::http::AttachmentType;
+
+#[command]
+#[aliases("explorer")]
+fn explorers(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let _ = msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Block Explorers :compass:");
+            e.description("List of block explorers that provide detailed information about Grin blocks and transactions.");
+            e.field("GrinExplorer", "https://grinexplorer.net/", false);
+            e.field("GrinScan", "https://grinscan.net/", false);
+            e.field("Blockscan", "https://grin.blockscan.com/", false);
+
+            e
+        });
+        m
+    });
+        
+    Ok(())
+}
+
+#[command]
+#[aliases("spending", "spendinglog")]
+fn spending_log(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let _ = msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Spending Log :pound:");
+            e.description("A log intended to keep the community up to date on how the Grin General Fund is being used.");
+            e.field("Spending Log", "https://github.com/mimblewimble/grin-pm/blob/master/financials/spending_log.csv", false);
+            e.field("Burn-rate & Runway Report", "https://github.com/mimblewimble/grin-pm/blob/master/financials/reports/burnrate_runway.md", false);
+
+            e
+        });
+        m
+    });
+
+    Ok(())
+}
+
+#[command]
+#[aliases("wallet")]
+fn wallets(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let _ = msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Wallets :purse:");
+            e.description("List of Grin wallets.");
+            e.image("attachment://grinplusplus.png");
+            e.field("Reference CLI Wallet (Windows, Mac, Linux)", "https://github.com/mimblewimble/grin-wallet/releases", false);
+            e.field("Grin++ GUI Wallet (Windows, Mac, Linux)", "https://grinplusplus.github.io/", false);
+            e.field("Niffler GUI Wallet (Windows, Mac, Linux)", "https://github.com/grinfans/niffler/releases/tag/v0.5.0", false);
+            e.field("Ironbelly Mobile Wallet (iOS, Android)","https://ironbelly.app/", false);
+            e.footer(|f| {
+                f.text("Grin++ Wallet v1.0.0");
+
+                f
+            });
+
+            e
+        });
+        m.add_file(AttachmentType::Path(Path::new("./assets/grinplusplus.png")));
+        m
+    });
+
+    Ok(())
+}

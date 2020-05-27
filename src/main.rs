f91eaee2b8d02851533ea5a631af32bb44eb4b6f
goyle <62769624+goyle@users.noreply.github.com>
2020-05-27 21:32:28 +0000

    Add funding_request command

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ struct Bot;
 struct Project;
 
 #[group]
-#[commands(obituary, explorers, spending_log, wallets)]
+#[commands(obituary, explorers, funding_request, spending_log, wallets)]
 struct Community;
 
 #[group]

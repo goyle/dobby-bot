b98ba30645390e9ffa1dd9b7688dd96c787c1217
goyle <62769624+goyle@users.noreply.github.com>
2020-05-27 21:42:15 +0000

    Add ignotus command

--- src/commands/people.rs
+++ src/commands/people.rs
@@ -1,1 +1,34 @@
+use std::path::Path;
 
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+use serenity::framework::standard::{
+    CommandResult,
+    macros::command,
+};
+use serenity::http::AttachmentType;
+
+#[command]
+#[aliases("igno", "Igno", "Ignotus")]
+fn ignotus(ctx: &mut Context, msg: &Message) -> CommandResult {
+    let _ = msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Ignotus Peverell :bust_in_silhouette: ");
+            e.description("The one and only, Ignotus Peverell, the anonymous founder of Grin. Left the project into the community's hands in June of 2019.");
+            e.image("attachment://ignotus.jpeg");
+            e.field("Github", "https://github.com/ignopeverell", false);
+            e.field("Grin Forum", "https://forum.grin.mw/u/igno.peverell/", false);
+            e.footer(|f| {
+                f.text("Ignotus Peverell");
+
+                f
+            });
+
+            e
+        });
+        m.add_file(AttachmentType::Path(Path::new("./assets/people/ignotus.jpeg")));
+        m
+    });
+        
+    Ok(())
+}
